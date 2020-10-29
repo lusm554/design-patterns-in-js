c63b0403262c9859d359fbc7ed58b49560569caf
loveyousomuch <loveyousomuch554@gmail.com>
2020-10-29 11:18:36 +0300

    open module

--- module.js
+++ module.js
@@ -10,3 +10,31 @@ const myModule = (function() {
 
 console.log(myModule.privateVar) // undefined
 myModule.privateMethod() // Hello World!
+
+
+// Open module
+const myRevealingModule = (function() {
+  let _ = 'some private value'
+  let publicVar = 'Hello World!'
+
+  function _func() {
+    console.log(`"_" = ${_}`)
+  }
+
+  function setPrivate(val) {
+    _ = val
+  }
+
+  function getPrivate() {
+    _func()
+  }
+
+  return {
+    setVar: setPrivate,
+    getVar: getPrivate,
+    greeting: publicVar
+  }
+})();
+
+myRevealingModule.setVar('new value!')
+myRevealingModule.getVar() // "_" = new value!
